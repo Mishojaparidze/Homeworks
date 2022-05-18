61f4b99fe47d1d23cbf84c4619ba8974c0ef1f37
misho <mishojaparidze@gmail.com>
2022-05-18 08:53:55 +0400

    new commit

--- Homework10/Homework10.cs
+++ Homework10/Homework10.cs
@@ -5,59 +5,6 @@ namespace Homework10
     internal class Program
     {
 
-        public interface FinanceOperations
-        {
-            int CalculateLoanPercent(int month);
-            void CheckUserHistory();
-        }
-
-        public class Bank : FinanceOperations
-        {
-            
-            public int CalculateLoanPercent(int n) 
-            {
-                var x = 5;
-                Console.WriteLine(n * x / 100 + n);
-                return n * x / 100 + n;
-            }
-            public void CheckUserHistory()
-            {
-                var random = new Random();
-                var y = random.Next(0,1001);
-                
-                if (y>500)
-                {
-                    Console.WriteLine(true);
-                    int n = Convert.ToInt32(Console.ReadLine());
-                    CalculateLoanPercent(n);
-                }
-            }
-
-        }
-
-
-        public class MicroFinance : FinanceOperations 
-        {
-
-            public int CalculateLoanPercent(int n)
-            {
-                var x = 10;
-                var k = 4;
-                Console.WriteLine(n * x / 100 + n*k+n);
-                return n * x / 100 + n;
-            }
-            public void CheckUserHistory()
-            {
-                int n = Convert.ToInt32(Console.ReadLine());
-                
-                Console.WriteLine("true");
-                CalculateLoanPercent(n);
-            }
-
-        }
-
-
-
         static void Main(string[] args)
         {
             #region Solution N1
