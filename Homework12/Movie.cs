6d5028e1d784422a6351acc5564d8359f4275465
misho <mishojaparidze@gmail.com>
2022-05-21 04:58:21 +0400

    second solution check

---
+++ Homework12/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12
+{
+    internal class Movie
+    {
+
+        public abstract class RealMovie
+        {
+            public abstract void Request();
+        }
+
+        public class RealActor : RealMovie
+        {
+            public override void Request()
+            {
+                Console.WriteLine("play in movie");
+            }
+        }
+        public class Stuntman : RealMovie
+        {
+
+            RealActor realActor;
+            public override void Request()
+            {
+                Random random = new Random();
+                var x=random.Next(0,101);
+                if (x>25)
+                    realActor = new RealActor();
+               realActor.Request();
+
+                Console.WriteLine("now you play in the movie");
+            }
+        }
+
+    }
+}
--- Homework12/Program.cs
+++ Homework12/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static Homework12.Concrete;
+using static Homework12.Movie;
 
 namespace Homework12
 {
@@ -8,12 +9,30 @@ namespace Homework12
 
         static void Main(string[] args)
         {
+            #region SolutionAbstractFactory
+            Console.WriteLine("SolutionAbstractFactory");
             Client client1 = new Client(new ArtDecoFactory());
             client1.Run();
             Client client2 = new Client(new VictorianFactory());
             client2.Run();
             Client client3 = new Client(new ModernFactory());
             client3.Run();
+            #endregion
+
+            #region SolutionProxy
+            Console.WriteLine("SolutionProxy");
+            RealMovie movie1 = new Stuntman();
+            movie1.Request();
+
+
+            RealActor actor = new RealActor();
+            actor.Request();
+
+            #endregion
+
+            #region SolutionFacade
+            Console.WriteLine("SolutionFacade");
+            #endregion
         }
     }
 }
