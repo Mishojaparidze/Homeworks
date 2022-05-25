11562c10545d6ac480b4de1d8808bcd6c48d0ee7
misho <mishojaparidze@gmail.com>
2022-05-25 22:03:49 +0400

    comment

---
+++ Homework12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static Homework12.Concrete;
+using static Homework12.Movie;
+
+namespace Homework12
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region SolutionAbstractFactory
+            Console.WriteLine("SolutionAbstractFactory");
+            Client client1 = new Client(new ArtDecoFactory());
+            client1.Run();
+            Client client2 = new Client(new VictorianFactory());
+            client2.Run();
+            Client client3 = new Client(new ModernFactory());
+            client3.Run();
+            #endregion
+
+            #region SolutionProxy
+            Console.WriteLine("SolutionProxy");
+
+            RealMovie movie1 = new Stuntman();
+            movie1.Request();
+
+            RealActor actor = new RealActor();
+            actor.Request();
+            #endregion
+
+            #region SolutionFacade
+            Console.WriteLine("SolutionFacade");
+            Facade facade = new Facade();
+            Console.WriteLine("HTML");
+            facade.OperationA();
+            Console.WriteLine("PDF");
+            facade.OperationB();
+            #endregion
+        }
+    }
+}
